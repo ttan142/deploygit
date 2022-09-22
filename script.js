5e8105e5e2056615763f19fc338a6abedb4e8726
ttan142 <trongtandoan@gmail.com>
2022-09-22 21:47:42 +0700

    update

--- script.js
+++ script.js
@@ -1,206 +1,187 @@
-var myUsername = document.getElementById("usrname");
-var myPsw = document.getElementById("psw");
-var myRePsw = document.getElementById("rePsw");
-var myEmail = document.getElementById("email");
-var letterUser = document.getElementById("letterUser");
-var letterPsw = document.getElementById("letterPsw");
-var capital = document.getElementById("capital");
-var length = document.getElementById("length");
-var letterRePsw = document.getElementById("letterRePsw");
-var capitalRePsw = document.getElementById("capitalRePsw");
-var lengthRePsw = document.getElementById("lengthRePsw");
-var submit = document.getElementById("submit");
-var repeat = document.getElementById("repeat");
-var info1 = document.getElementById("message1");
-var info2 = document.getElementById("message2");
-var info3 = document.getElementById("message3");
-var info4 = document.getElementById("message4");
-var togglePassword = document.getElementById("togglePassword");
-var togglePassword2 = document.getElementById("togglePassword2");
+const $ = document.getElementById.bind(document);
+const myUsername = $("usrname");
+const myPsw = $("psw");
+const myRePsw = $("rePsw");
+const myEmail = $("email");
+const letterUser = $("letterUser");
+const letterPsw = $("letterPsw");
+const capital = $("capital");
+const length = $("length");
+const letterRePsw = $("letterRePsw");
+const capitalRePsw = $("capitalRePsw");
+const lengthRePsw = $("lengthRePsw");
+const submit = $("submit");
+const repeat = $("repeat");
+const info1 = $("message1");
+const info2 = $("message2");
+const info3 = $("message3");
+const info4 = $("message4");
+const togglePassword = $("togglePassword");
+const togglePassword2 = $("togglePassword2");
 
-function showPassword() {
-  if (myPsw.type === "password" && myRePsw.type === "password") {
-    myPsw.type = "text";
-    myRePsw.type = "text";
-  } else {
-    myPsw.type = "password";
-    myRePsw.type = "password";
-  }
-  togglePassword.classList.toggle("fa-eye-slash");
-  togglePassword2.classList.toggle("fa-eye-slash");
-}
+const showPassword = (x, y) => {
+  x.type === "password" ? (x.type = "text") : (x.type = "password");
+  y.classList.toggle("fa-eye-slash");
+};
 
-function show(x) {
+const show = (x) => {
   x.style.display = "block";
-}
-function hide(x) {
+};
+const hide = (x) => {
   x.style.display = "none";
-}
+};
 
-function disable(x) {
+const disable = (x) => {
   x.classList.remove("valid");
   x.classList.add("invalid");
   show(x);
   finalCheking();
-}
+};
 
-function enable(x) {
+const enable = (x) => {
   x.classList.remove("invalid");
   x.classList.add("valid");
   hide(x);
   finalCheking();
-}
+};
 
-function check(x) {
-  return x.classList.contains("valid");
-}
+const check = (x) => x.classList.contains("valid");
 
-function verifyPassword() {
+const verifyPassword = () => {
   alert("Đã hoàn thành form thành công.");
-}
+};
 
-function finalCheking() {
-  if (
-    check(letterPsw) &&
-    check(capital) &&
-    check(length) &&
-    check(email1) &&
-    check(repeat) &&
-    check(letterUser)
-  ) {
-    submit.disabled = false;
-    submit.style.opacity = 1;
-  } else {
-    submit.disabled = true;
-    submit.style.opacity = 0.4;
-  }
-}
+const finalCheking = () => {
+  check(letterPsw) &&
+  check(capital) &&
+  check(length) &&
+  check(email1) &&
+  check(repeat) &&
+  check(letterUser)
+    ? ((submit.disabled = false), (submit.style.opacity = 1))
+    : ((submit.disabled = true), (submit.style.opacity = 0.4));
+};
+
+const checkUserName = () => {
+  const lowerCaseLetters1 = /^[^@$!%*#?&^_-]*$/g;
+  myUsername.value.match(lowerCaseLetters1) && myUsername.value.length !== 0
+    ? (hide(info1), enable(letterUser))
+    : (show(info1), disable(letterUser));
+};
+
+const checkEmail = () => {
+  const lowerCaseLetters2 = /^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{1,}$/g;
+  myEmail.value.match(lowerCaseLetters2)
+    ? (hide(info2), enable(email1))
+    : (show(info2), disable(email1));
+};
+
+const checkBasicPsw = (x, letter, capital, length) => {
+  const lowerCaseLetters = /[a-z]/g;
+  const upperCaseLetters = /[A-Z]/g;
+  x.value.match(lowerCaseLetters) ? enable(letter) : disable(letter);
+
+  x.value.match(upperCaseLetters) ? enable(capital) : disable(capital);
+
+  x.value.length >= 8 && x.value.length <= 32
+    ? enable(length)
+    : disable(length);
+};
+
+const checkPsw = () => {
+  checkBasicPsw(myPsw, letterPsw, capital, length);
+  check(letterPsw) && check(capital) && check(length)
+    ? hide(info3)
+    : show(info3);
+};
+
+const checkRePsw = () => {
+  checkBasicPsw(myRePsw, letterRePsw, capitalRePsw, lengthRePsw);
+
+  myRePsw.value === myPsw.value ? enable(repeat) : disable(repeat);
+
+  check(letterRePsw) &&
+  check(capitalRePsw) &&
+  check(lengthRePsw) &&
+  check(repeat)
+    ? hide(info4)
+    : show(info4);
+};
 
 submit.disabled = true;
 
-myUsername.onfocus = function () {
+myUsername.onfocus = () => {
   show(info1);
+  checkUserName();
 };
 
-myUsername.onblur = function () {
+myUsername.onblur = () => {
   hide(info1);
+  checkUserName();
 };
 
-myUsername.onkeyup = function () {
-  var lowerCaseLetters1 = /^[^@$!%*#?&^_-]*$/g;
-  if (
-    myUsername.value.match(lowerCaseLetters1) &&
-    myUsername.value.length !== 0
-  ) {
-    hide(info1);
-    enable(letterUser);
-  } else {
-    show(info1);
-    disable(letterUser);
-  }
+myUsername.oninput = () => {
+  checkUserName();
 };
 
-myEmail.onfocus = function () {
+myEmail.onfocus = () => {
   show(info2);
+  checkEmail();
 };
 
-myEmail.onblur = function () {
+myEmail.onblur = () => {
   hide(info2);
+  checkEmail();
 };
 
-myEmail.onkeyup = function () {
-  var lowerCaseLetters2 = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{1,}$/g;
-  if (myEmail.value.match(lowerCaseLetters2)) {
-    hide(info2);
-    enable(email1);
-  } else {
-    show(info2);
-    disable(email1);
-  }
+myEmail.oninput = () => {
+  checkEmail();
 };
 
-myPsw.onfocus = function () {
+myPsw.onfocus = () => {
   show(info3);
+  checkPsw();
+  checkRePsw();
 };
 
-myPsw.onblur = function () {
+myPsw.onblur = () => {
   hide(info3);
+  checkPsw();
+  checkRePsw();
 };
 
-myPsw.onkeyup = function () {
-  var lowerCaseLetters = /[a-z]/g;
-  var upperCaseLetters = /[A-Z]/g;
-  if (myPsw.value.match(lowerCaseLetters)) {
-    enable(letterPsw);
-  } else {
-    disable(letterPsw);
-  }
-
-  if (myPsw.value.match(upperCaseLetters)) {
-    enable(capital);
-  } else {
-    disable(capital);
-  }
-
-  if (myPsw.value.length >= 8 && myPsw.value.length <= 32) {
-    enable(length);
-  } else {
-    disable(length);
-  }
-
-  if (check(letterPsw) && check(capital) && check(length)) {
-    hide(info3);
-  } else {
-    show(info3);
-  }
+myPsw.oninput = () => {
+  checkPsw();
+  checkRePsw();
 };
 
-togglePassword.onclick = function () {
-  showPassword();
+togglePassword.onclick = () => {
+  showPassword(myPsw, togglePassword);
 };
 
-myRePsw.onfocus = function () {
+myRePsw.onfocus = () => {
   show(info4);
+  checkRePsw();
 };
 
-myRePsw.onblur = function () {
+myRePsw.onblur = () => {
   hide(info4);
+  checkRePsw();
 };
 
-myRePsw.onkeyup = function () {
-  var lowerCaseLetters = /[a-z]/g;
-  var upperCaseLetters = /[A-Z]/g;
-  if (myRePsw.value.match(lowerCaseLetters)) {
-    enable(letterRePsw);
-  } else {
-    disable(letterRePsw);
-  }
-
-  if (myRePsw.value.match(upperCaseLetters)) {
-    enable(capitalRePsw);
-  } else {
-    disable(capitalRePsw);
-  }
-
-  if (myRePsw.value.length >= 8 && myPsw.value.length <= 32) {
-    enable(lengthRePsw);
-  } else {
-    disable(lengthRePsw);
-  }
-
-  if (myRePsw.value === myPsw.value) {
-    enable(repeat);
-  } else {
-    disable(repeat);
-  }
-
-  if (check(letterRePsw) && check(capital) && check(length) && check(repeat)) {
-    hide(info4);
-  } else {
-    show(info4);
-  }
+myRePsw.oninput = () => {
+  checkRePsw();
 };
 
-togglePassword2.onclick = function () {
-  showPassword();
+togglePassword2.onclick = () => {
+  showPassword(myRePsw, togglePassword2);
 };
+
+fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then((response) => response.json())
+  .then((data) => {
+    myUsername.value = data.username;
+    myEmail.value = data.email;
+    myPsw.value = data.name;
+    myRePsw.value = data.name;
+  });
